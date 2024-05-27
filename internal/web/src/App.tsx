c395f8dc9ff50c6c9b2a13c1b0a09c8d6397220a
ItsNotGoodName <gurnaindeol@gmail.com>
2024-05-27 02:45:43 -0700

    feat: tanstack query

--- internal/web/src/App.tsx
+++ internal/web/src/App.tsx
@@ -1,14 +1,40 @@
+import { createQuery } from "@tanstack/solid-query"
+import { createQueryKeyStore } from "@lukemorales/query-key-factory";
 import { provideTheme } from "./ui/theme"
+import { ErrorBoundary, Suspense } from "solid-js"
+
+
+export const queries = createQueryKeyStore({
+  github: {
+    tanstack: null
+  }
+});
 
 function App() {
   provideTheme()
 
+  const repositoryQuery = createQuery(() => ({
+    ...queries.github.tanstack,
+    queryFn: async () => {
+      const result = await fetch('https://api.github.com/repos/TanStack/query')
+      if (!result.ok) throw new Error('Failed to fetch data')
+      return result.json()
+    },
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    throwOnError: true, // Throw an error if the query fails
+  }))
+
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div>
+      <div>Static Content</div>
+      {/* An error while fetching will be caught by the ErrorBoundary */}
+      <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+        {/* Suspense will trigger a loading state while the data is being fetched */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <div>{repositoryQuery.data?.updated_at}</div>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
   )
 }
 
--- internal/web/src/index.tsx
+++ internal/web/src/index.tsx
@@ -3,7 +3,15 @@ import { render } from 'solid-js/web'
 
 import "./styles/index.css";
 import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query';
+import { SolidQueryDevtools } from '@tanstack/solid-query-devtools'
 
 const root = document.getElementById('root')
+const client = new QueryClient()
 
-render(() => <App />, root!)
+render(() =>
+  <QueryClientProvider client={client}>
+    <SolidQueryDevtools initialIsOpen={false} />
+    <App />
+  </QueryClientProvider>
+  , root!)
