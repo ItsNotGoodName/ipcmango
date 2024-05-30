30bc96060bc67a8f224ab05df0a53fad511ae9c9
ItsNotGoodName <gurnaindeol@gmail.com>
2024-05-30 14:25:29 -0700

    gob

--- internal/core/queue.go
+++ internal/core/queue.go
@@ -1,9 +1,10 @@
 package core
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
-	"encoding/json"
+	"encoding/gob"
 
 	"github.com/maragudk/goqite"
 	"github.com/maragudk/goqite/jobs"
@@ -21,25 +22,25 @@ type JobBuilder[T any] struct {
 }
 
 func (e JobBuilder[T]) Create(ctx context.Context, q *goqite.Queue, data T) error {
-	b, err := json.Marshal(data)
-	if err != nil {
+	var buffer bytes.Buffer
+	if err := gob.NewEncoder(&buffer).Encode(data); err != nil {
 		return err
 	}
-	return jobs.Create(ctx, q, e.Action, b)
+	return jobs.Create(ctx, q, e.Action, buffer.Bytes())
 }
 
 func (e JobBuilder[T]) CreateTx(ctx context.Context, tx *sql.Tx, q *goqite.Queue, data T) error {
-	b, err := json.Marshal(data)
-	if err != nil {
+	var buffer bytes.Buffer
+	if err := gob.NewEncoder(&buffer).Encode(data); err != nil {
 		return err
 	}
-	return jobs.CreateTx(ctx, tx, q, e.Action, b)
+	return jobs.CreateTx(ctx, tx, q, e.Action, buffer.Bytes())
 }
 
 func (e JobBuilder[T]) Register(ctx context.Context, r *jobs.Runner, job func(ctx context.Context, data T) error) {
 	r.Register(e.Action, func(ctx context.Context, m []byte) error {
 		var data T
-		if err := json.Unmarshal(m, &data); err != nil {
+		if err := gob.NewDecoder(bytes.NewReader(m)).Decode(&data); err != nil {
 			return err
 		}
 		return job(ctx, data)
